afae4dac076047843c6a408170319b0f9f2e8b4d
Niwanthi Edirisinghe <niwanthiedirisinghe.95@gmail.com>
2024-05-27 00:17:42 +1000

    Hide the modal

--- Frontend/js/feedback.js
+++ Frontend/js/feedback.js
@@ -1,39 +1,44 @@
 $("#submit-feedback").click(function (event) {
-    event.preventDefault();
-  
-    var form = document.querySelector("#feedback-form");
-  
-    // Validation
-    // if (form.checkValidity() === false) {
-    //   event.stopPropagation();
-    //   form.classList.add("was-validated");
-    //   return;
-    // }
-   
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:3000/api/feedback/create",
-      data: JSON.stringify({ comment: $("#comment").val() }),
-      contentType: "application/json; charset=utf-8",
-      processData: false,
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("token")
-      },
-      success: function (response) {
-        $("#feedbackModal").modal("hide");
-  
-        Swal.fire({
-          icon: "success",
-          title: "Feedback added successfully!",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-  
-        // loadFeedback();
-      },
-      error: function (error) {
-        console.error("Error:", error);
-        alert("Failed to add feedback.");
-      },
-    });
+  event.preventDefault();
+
+  var form = document.querySelector("#feedback-form");
+
+  // Validation
+  // if (form.checkValidity() === false) {
+  //   event.stopPropagation();
+  //   form.classList.add("was-validated");
+  //   return;
+  // }
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:3000/api/feedback/create",
+    data: JSON.stringify({ comment: $("#comment").val() }),
+    contentType: "application/json; charset=utf-8",
+    processData: false,
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+    success: function (response) {
+      //$("#feedbackModal").modal("hide");
+
+      Swal.fire({
+        icon: "success",
+        title: "Feedback added successfully!",
+        showConfirmButton: false,
+        timer: 1500,
+      }).then(() => {
+        // Reset the feedback form
+        form.reset();
+        // Hide the feedback modal
+        $("#feedbackRecipeModal").modal("hide");
+      });
+
+      // loadFeedback();
+    },
+    error: function (error) {
+      console.error("Error:", error);
+      alert("Failed to add feedback.");
+    },
+  });
 });
